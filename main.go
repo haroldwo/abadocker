c7fad25a396ecf3e2fd3b6c315ecb88bfc2156da
Fred Wong <645939756@qq.com>
2018-09-16 11:21:50 -0700

    revise memory limit code

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+const cgroupMemHieMnt = "/sys/fs/cgroup/memory" //default Hierarchy of memory subsystem
+
+func main() {
+	if os.Args[0] == "/proc/self/exe" { //The static resource file of current process
+		fmt.Printf("current pid %d", syscall.Getpid())
+		os.Mkdir(path.Join(cgroupMemHieMnt, "testmemlmt"), 0755)
+		ioutil.WriteFile(path.Join(cgroupMemHieMnt, "testmemlmt", "tasks"), []byte(strconv.Itoa(syscall.Getpid())), 0644) //Add the host process info in Cgroup
+		ioutil.WriteFile(path.Join(cgroupMemHieMnt, "testmemlmt", "memory.limit_in_bytes"), []byte("100m"), 0644)
+		fmt.Println()
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	cmd := exec.Command("/proc/self/exe")
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	cmd.Process.Wait()
+}
